716c3de23c05e38b6938d85544682478e3fe6545
Reza Lael <rezalaal@gmail.com>
2025-10-25 16:03:35 +0330

    search users

--- app/Livewire/Ambassadors.php
+++ app/Livewire/Ambassadors.php
@@ -5,23 +5,32 @@ namespace App\Livewire;
 use App\Models\User;
 use Livewire\Component;
 use Livewire\WithPagination;
-use phpDocumentor\Reflection\Types\Null_;
 
 class Ambassadors extends Component
 {
     use WithPagination;
 
+    public $search = '';
+
     protected $paginationTheme = 'tailwind';
+
+    public function updatingSearch()
+    {
+        // وقتی سرچ تغییر می‌کنه، به صفحه ۱ برگرد
+        $this->resetPage();
+    }
 
     public function render()
     {
         $users = User::select('first_name', 'last_name', 'code', 'job_title')
-    ->where(function ($q) {
-        $q->where('email', '<>', 'admin@local.tld')
-            ->orWhereNull('email');
-        })
-        ->paginate(10);
-
+            ->where(function ($q) {
+                $q->where('email', '<>', 'admin@local.tld')
+                  ->orWhereNull('email');
+            })
+            ->when($this->search, function ($q) {
+                $q->where('last_name', 'like', '%'.$this->search.'%');
+            })
+            ->paginate(10);
 
         return view('livewire.ambassadors', [
             'users' => $users,
