39b2b9d59093eaed693c33e888b11b97fa581a92
Reza Lael <rezalaal@gmail.com>
2025-10-27 01:40:05 +0330

    update composer- liara.json and livewire config

--- config/livewire.php
+++ config/livewire.php
@@ -50,7 +50,7 @@ return [
     |
     */
 
-    'lazy_placeholder' => null,
+    'lazy_placeholder' => null, /*'livewire.placeholder',
 
     /*
     |---------------------------------------------------------------------------
@@ -64,17 +64,17 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => 'public',        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => 'file|mimes:png,jpg|max:102400',       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => 'livewire-tmp',   // Example: 'tmp'                      | Default: 'livewire-tmp'
-        'middleware' => ['web'],  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
+        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
         'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
-        'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
+        'cleanup' => true,
     ],
 
     /*
