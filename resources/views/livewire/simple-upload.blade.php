8de6ed215613663ea7278d2141f9883f1ce3a034
Reza Lael <rezalaal@gmail.com>
2025-10-27 00:39:12 +0330

    changed mary to simple input

--- resources/views/livewire/simple-upload.blade.php
+++ resources/views/livewire/simple-upload.blade.php
@@ -1,5 +1,11 @@
 <div>
-    <x-file wire:model.live="photo" accept="image/png, image/jpeg">
-        <img src="{{ $user->avatar ?? asset('images/avatar.png') }}" class="h-40 rounded-lg" />
-    </x-file>
+    <div>
+        <input type="file" wire:model.live="photo" accept="image/png,image/jpeg">
+        @if($photo)
+            <img src="{{ $photo->temporaryUrl() }}" class="h-40 rounded-lg">
+        @else
+            <img src="{{ $user->avatar ?? asset('images/avatar.png') }}" class="h-40 rounded-lg">
+        @endif
+    </div>
+
 </div>
